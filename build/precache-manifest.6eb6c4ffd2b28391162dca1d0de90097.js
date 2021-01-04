eceb4b5a616bb3655c60e1acd7611b714240b0b1
Vimaleshwar <58393075+Vimaleshwar@users.noreply.github.com>
2021-01-04 17:18:08 +0530

    Add files via upload

---
+++ build/precache-manifest.6eb6c4ffd2b28391162dca1d0de90097.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9628f46435fbaa9ab8724f2fb9f27817",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c64bca31ebe41be7c0b5",
+    "url": "/static/css/main.9044cdec.chunk.css"
+  },
+  {
+    "revision": "e5042de19ad40608a1f2",
+    "url": "/static/js/2.83696ff5.chunk.js"
+  },
+  {
+    "revision": "c64bca31ebe41be7c0b5",
+    "url": "/static/js/main.a3bf3707.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  },
+  {
+    "revision": "ea3d17e94cb8ccd393dc727360349acc",
+    "url": "/static/media/Back.ea3d17e9.svg"
+  },
+  {
+    "revision": "61cf76fdd143a014b1f4f669c675cc83",
+    "url": "/static/media/board.61cf76fd.svg"
+  },
+  {
+    "revision": "1193c50a3862e1127caa43768cd71f8b",
+    "url": "/static/media/card-newone.1193c50a.jpg"
+  },
+  {
+    "revision": "2a8153ef719d8cb58b576262b1e181e3",
+    "url": "/static/media/hero-a.2a8153ef.svg"
+  }
+]);
